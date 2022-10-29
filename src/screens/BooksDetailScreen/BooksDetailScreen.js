b7538435e2ec086949ddd73931fe4d653eda66ae
Eduardo Joaquin <joaquincespedeseduardo@gmail.com>
2022-10-29 19:31:46 -0400

    Creation of BooksScreen and BooksDetailScreen

---
+++ src/screens/BooksDetailScreen/BooksDetailScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const BooksDetailScreen = () => {
+  return (
+    <View>
+      <Text>BooksDetailScreen</Text>
+    </View>
+  );
+};
+
+export default BooksDetailScreen;
---
+++ src/screens/BooksScreen/BooksScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const BooksScreen = () => {
+  return (
+    <View>
+      <Text>BooksScreen</Text>
+    </View>
+  );
+};
+
+export default BooksScreen;
